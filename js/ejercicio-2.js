e9561d850298772f5f0d26f8c9d08b80910ef52c
Lucasfi <lukafigueroa64@gmail.com>
2025-05-04 02:22:06 -0300

    agregamos prompt y condiciones en los metodos para interaccion del usuario para ingresar y extraer el dinero de cuenta

--- js/ejercicio-2.js
+++ js/ejercicio-2.js
@@ -12,19 +12,38 @@ Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y vo
 const cuenta = {
     titular: 'Alex',
     saldo: 0,
-    ingresar: function(cantidad){
-        this.saldo += cantidad;
-        document.writeln('<h3>Se ha ingresado $'+cantidad+' a la cuenta</h3>');
+    ingresar: function(){
+        const cantidad = parseInt(prompt('Ingrese la cantidad a depositar'));
+        if(!isNaN(cantidad) && cantidad > 0) {
+            this.saldo += cantidad;
+            alert(`Se ingresaron ${cantidad}, Nuevo saldo: ${this.saldo}`)
+        } else {
+            alert('Ingrese una cantidad válida')
+        }
+        document.writeln(`<h3>Se ha ingresado ${cantidad} a la cuenta</h3>`);
     }
-    ,extraer: function(cantidad){
-        this.saldo -= cantidad;
-        document.writeln('<h3>Se ha retirado $'+cantidad+' de la cuenta</h3>');
+    ,extraer: function(){
+        const cantidad = parseInt(prompt('Ingrese la cantidad a retirar'));
+        if(!isNaN(cantidad) && cantidad >= cantidad) {
+            if(this.saldo >= cantidad) {
+                this.saldo -= cantidad;
+                alert(`Se retiraron ${cantidad}, Nuevo saldo: ${this.saldo}`);
+            } else {
+                alert('Saldo insuficiente');
+            }
+        } else {
+            alert('Ingrese una cantidad válida');
+        }
+        document.writeln(`<h3>Se ha retirado ${cantidad} de la cuenta</h3>`);
     }
     ,informar: function(){
-        document.writeln('<h3>El saldo de la cuenta es de $'+this.saldo+'</h3>');
+        document.writeln(`<h3>El saldo de la cuenta es de:  $${this.saldo}</h3>`);
     }
 
 }
 
-document.writeln('<h3>El titular de la cuenta es '+cuenta.titular+'</h3>');
+document.writeln(`<h3>El titular de la cuenta es: ${cuenta.titular}</h3>`);
+cuenta.informar();
+cuenta.ingresar();
+cuenta.extraer();
 cuenta.informar();
