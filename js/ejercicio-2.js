805dd486f598fa31755e3636842a6543fd8ad425
Lucasfi <lukafigueroa64@gmail.com>
2025-05-03 23:04:15 -0300

    logramos crear objeto y metodos y mostrar saldo

---
+++ js/ejercicio-2.js
@@ -0,0 +1,30 @@
+/*
+Escribe un programa que cree un objeto "cuenta" con las siguientes propiedades:
+Una propiedad titular con el valor "Alex".
+Una propiedad saldo, teniendo como valor inicial 0.
+Un método ingresar() que permita añadir dinero a la cuenta, pasando la cantidad como parámetro
+Un método extraer() que permita retirar la cantidad pasada como parámetro.
+Un método informar() que retorne la información del estado de la cuenta. 
+
+Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a mostrar la descripción del estado de la cuenta.
+*/
+
+const cuenta = {
+    titular: 'Alex',
+    saldo: 0,
+    ingresar: function(cantidad){
+        this.saldo += cantidad;
+        document.writeln('<h3>Se ha ingresado $'+cantidad+' a la cuenta</h3>');
+    }
+    ,extraer: function(cantidad){
+        this.saldo -= cantidad;
+        document.writeln('<h3>Se ha retirado $'+cantidad+' de la cuenta</h3>');
+    }
+    ,informar: function(){
+        document.writeln('<h3>El saldo de la cuenta es de $'+this.saldo+'</h3>');
+    }
+
+}
+
+document.writeln('<h3>El titular de la cuenta es '+cuenta.titular+'</h3>');
+cuenta.informar();
